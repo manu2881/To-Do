c9612cb45871bb7815f21c2634731d1eb670287a
manu2881 <65648759+manu2881@users.noreply.github.com>
2023-07-06 09:48:26 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# To-Do
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-i9lpur)
---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+function TodoApp() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const handleAddTodo = () => {
+    if (newTodo.trim() !== '') {
+      setTodos([...todos, newTodo]);
+      setNewTodo('');
+    }
+  };
+
+  return (
+    <div>
+      <h1>Todo App</h1>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoApp;
